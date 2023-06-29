bfe39c14a1bcde93000788ed3beb6ca7fc4cdd7b
hushin <ngtv.hsn@gmail.com>
2023-06-29 09:25:15 +0000

    vee-validate

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <input v-model="value" placeholder="Type something" />
+  <span>{{ errorMessage }}</span>
 </template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate'
+
+function validateField(value) {
+  if (!value) {
+    return 'this field is required'
+  }
+
+  if (value.length < 8) {
+    return 'this field must contain at least 8 characters'
+  }
+
+  return true
+}
+
+const { value, errorMessage } = useField('fullName', validateField)
+</script>
+
+<style>
+input,
+span {
+  display: block;
+  font-size: 16px;
+}
+</style>
